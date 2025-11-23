f2ce76f654ad5de7fc9173efedeeb14d31c25b62
Lylnia <lylnia.dev@gmail.com>
2025-11-23 15:26:40 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // Manifest dosyasının yolu. 
 // Vercel'e atınca kendi domainin ile güncellersen iyi olur ama şu an da çalışır.
-const manifestUrl = "/tonconnect-manifest.json";
+const manifestUrl = "public/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
